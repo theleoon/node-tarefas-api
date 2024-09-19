5d16bd5336df4db7e31ae70f055d9215e25deeef
Leonardo Inacio <leonardo.inacio@caelum.com.br>
2024-09-19 17:03:09 -0300

    Adicionando a busca por título com regex

--- src/controllers/tarefaController.js
+++ src/controllers/tarefaController.js
@@ -10,6 +10,26 @@ class TarefaController {
         res.status(500).json({ message: `${erro.message} - falha na requisição` });
       }
     };
+
+    static async buscaTarefaPorId (req, res) {
+      try {
+        const id = req.params.id;
+        const encontrado = await tarefa.findById(id);
+        res.status(200).json(encontrado);
+      } catch (erro) {
+        res.status(500).json({ message: `${erro.message} - falha na requisição` });
+      }
+    };
+
+    static async buscaTarefaPorTitulo (req, res) {
+      const titulo = req.query.titulo;
+      try {
+        const tarefas = await tarefa.find({ titulo: { $regex: titulo, $options: 'i' } });
+        res.status(200).json(tarefas);
+      } catch (erro) {
+        res.status(500).json({ message: `${erro.message} - falha na busca` });
+      }
+    };
 }
 
 export default TarefaController;
--- src/routes/tarefasRoutes.js
+++ src/routes/tarefasRoutes.js
@@ -4,5 +4,7 @@ import TarefaController from "../controllers/tarefaController.js";
 const routes = express.Router();
 
 routes.get("/tarefas", TarefaController.listarTarefas);
+routes.get("/tarefas/busca", TarefaController.buscaTarefaPorTitulo);
+routes.get("/tarefas/:id", TarefaController.buscaTarefaPorId);
 
 export default routes;
